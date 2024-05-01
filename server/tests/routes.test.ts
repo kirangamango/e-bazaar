07fc85882842d8bd47766d32fa649671fd121eee
Kiran Kumar <kirangamango@gmail.com>
2024-05-01 14:32:09 +0530

    jest setup

--- server/tests/routes.test.ts
+++ server/tests/routes.test.ts
@@ -1,6 +1,8 @@
 import { UserFunction } from "../functions";
 
 test("get all should return 200 status response", async () => {
+  console.log("inside test-------------------------------->");
   const data = await UserFunction.getAll({});
+  console.log({ userData: data.users[0] });
   expect(data).toBeTruthy();
 });
