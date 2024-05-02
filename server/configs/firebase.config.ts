7cdcb2128c836c95445858464952c27b34437baf
Kiran Kumar <kirangamango@gmail.com>
2024-05-02 11:55:40 +0530

    test

--- server/configs/firebase.config.ts
+++ server/configs/firebase.config.ts
@@ -1,6 +1,12 @@
 import admin from "firebase-admin";
 import { configs } from ".";
 
+console.log({
+  email: configs.CLIENT_EMAIL,
+  privatekey: configs.PRIVATE_KEY,
+  projectId: configs.PROJECT_ID,
+});
+
 admin.apps.length === 0 &&
   admin.initializeApp({
     credential: admin.credential.cert({
